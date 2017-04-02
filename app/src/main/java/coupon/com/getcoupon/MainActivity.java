6cfc77ab64e7990e5c41676d5d0d0369d7b3cdcb
NgocDh <th113023@gmail.com>
2017-04-02 22:53:28 +0700

    second commit

--- app/src/main/java/coupon/com/getcoupon/MainActivity.java
+++ app/src/main/java/coupon/com/getcoupon/MainActivity.java
@@ -1,13 +1,57 @@
 package coupon.com.getcoupon;
 
+import android.content.res.Resources;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import coupon.com.getcoupon.widget.DrawerArrowDrawable;
+import static android.view.Gravity.START;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerArrowDrawable drawerArrowDrawable;
+    private float offset;
+    private boolean flipped;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ImageView imageView = (ImageView) findViewById(R.id.drawer_indicator);
+        final Resources resources = getResources();
+
+        drawerArrowDrawable = new DrawerArrowDrawable(resources);
+        drawerArrowDrawable.setStrokeColor(resources.getColor(R.color.light_gray));
+        imageView.setImageDrawable(drawerArrowDrawable);
+
+        drawer.setDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override public void onDrawerSlide(View drawerView, float slideOffset) {
+                offset = slideOffset;
+
+                // Sometimes slideOffset ends up so close to but not quite 1 or 0.
+                if (slideOffset >= .995) {
+                    flipped = true;
+                    drawerArrowDrawable.setFlip(flipped);
+                } else if (slideOffset <= .005) {
+                    flipped = false;
+                    drawerArrowDrawable.setFlip(flipped);
+                }
+
+                drawerArrowDrawable.setParameter(offset);
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if (drawer.isDrawerVisible(START)) {
+                    drawer.closeDrawer(START);
+                } else {
+                    drawer.openDrawer(START);
+                }
+            }
+        });
+
     }
 }
