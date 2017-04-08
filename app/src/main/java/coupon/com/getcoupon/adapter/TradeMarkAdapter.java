e41ac10454de34190f6f2334f37dfa120914f781
Ngocdh <ngoc.dh@citigo.com.vn>
2017-04-08 14:13:18 +0700

    Di ve

---
+++ app/src/main/java/coupon/com/getcoupon/adapter/TradeMarkAdapter.java
@@ -0,0 +1,32 @@
+package coupon.com.getcoupon.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ngocdh on 4/8/17.
+ */
+
+public class TradeMarkAdapter extends RecyclerView.Adapter<TradeMarkAdapter.TradeMarkViewHolder> {
+    @Override
+    public TradeMarkViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(TradeMarkViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class TradeMarkViewHolder extends RecyclerView.ViewHolder {
+        public TradeMarkViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
